cd2b1697fc5df40044061adca622ec264697d7d4
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-27 18:14:10 +0700

    add hero

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,17 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.scss']
+  styleUrls: ['./heroes.component.css']
 })
+
 export class HeroesComponent implements OnInit {
 
-  hero = 'Winstorm';
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
 
   constructor() { }
 
-  ngOnInit(): void { //ดึงข้อลจาก api ใช้ตรงนี้
+  ngOnInit() {
   }
 
 }
