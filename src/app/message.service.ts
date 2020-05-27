ee03eace8ec9014e40382eac059e5dbd3a3e3759
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-27 20:27:07 +0700

    add message service

--- src/app/message.service.ts
+++ src/app/message.service.ts
@@ -3,7 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
+
 export class MessageService {
 
+  messages: string[] = [];
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+  //เพิ่มข้อมูล
+
+  clear() {
+    this.messages = [];
+  }
+  //ลบข้อมูล
   constructor() { }
 }
