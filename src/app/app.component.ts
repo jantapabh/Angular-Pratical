121b82d475f11b412ed63fc8166a197a8898bf26
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-27 17:36:10 +0700

    add commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html', //บอกว่าไฟล์ app ที่ต้องการใช้อยู่ที่ไหน
+  selector: 'app-root', //ตัวบอกว่าจะเรียกใช้ตรงไหย ยังไง
+  templateUrl: './app.component.html', //บอกว่าไฟล์ app ที่ต้องการใช้อยู่ที่ไหน เหมือนอ้างอิงให้ไปแสดงที่ไฟล์ app.component.html
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent { //เป็นคลาสที่ถูกมองว่าเป็น attribute
