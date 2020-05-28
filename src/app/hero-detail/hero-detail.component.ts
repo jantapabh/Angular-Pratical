38139fbc479b0cb76553a7304ad97c82dc5355fa
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-28 13:16:35 +0700

    add commit

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Hero } from '../hero';
 
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
+import { HeroService }  from '../hero.service';
+
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
@@ -10,9 +15,25 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;  //ทำให้ hero เป็นตัวแปรประเภท input
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
+    this.getHero();
+  }
+
+  //เพิ่มฟังก์ชั่นสำหรับ getHero
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  //เพิ่มฟังก์ชั่นสำหรับกลับ
+  goBack(): void {
+    this.location.back();
   }
 
 }
