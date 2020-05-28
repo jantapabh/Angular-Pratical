29dca86f4af42ac649936d140ce3d8c714fb7be0
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-28 13:53:03 +0700

    add app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 
+//import HTTP
+import { HttpClientModule }    from '@angular/common/http';
+
 //import component
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -10,6 +13,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
+//import Web API
+import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 @NgModule({
   declarations: [ //เป็นการบอกว่ามี components อะไรบ้าง
@@ -22,7 +28,10 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     
   ],
   imports: [ //เป็นการดึงโมดูลมาใช้
-    
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    ),
     BrowserModule,
     AppRoutingModule,
     FormsModule //import form เพื่อสามารถเปลี่ยนค่าในฟอร์มได้
@@ -30,8 +39,11 @@ import { DashboardComponent } from './dashboard/dashboard.component';
   providers: [],
   bootstrap: [AppComponent] //เป็นการกำหนดจุดเริ่มต้นของ app
   
+  
 })
-export class AppModule { }
+export class AppModule {
+
+ }
 
 
 // ใน form module มี directive ชื่อ ngModel อยู่
