d0acaf30402cbd2d5ee1a18634732954c9a87955
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-28 12:49:21 +0700

    add dashborad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,14 @@ import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @NgModule({
   declarations: [ //เป็นการบอกว่ามี components อะไรบ้าง
 
     AppComponent, 
-    HeroesComponent, HeroDetailComponent, MessagesComponent,
+    HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent,
     
   ],
   imports: [ //เป็นการดึงโมดูลมาใช้
