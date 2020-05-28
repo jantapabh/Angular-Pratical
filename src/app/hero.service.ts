0cdf400025ad5656f2ed0bcb5a4adffa4203b06f
jantapabh <s6035512034@phuket.psu.ac.th>
2020-05-28 14:07:52 +0700

    add hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -65,6 +65,16 @@ export class HeroService {
     };
   }
 
+  //ส่วนเพิ่มข้อมูล
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+
 
 }
 
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -21,4 +21,15 @@ export class HeroesComponent implements OnInit {
     this.heroService.getHeroes()
     .subscribe(heroes => this.heroes = heroes);
   }
+ 
+
+  //ส่วนเพิ่ม hero
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.addHero({ name } as Hero)
+      .subscribe(hero => {
+        this.heroes.push(hero);
+      });
+  }
 }
