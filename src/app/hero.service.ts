3302d962bc2a20918710ebc0d6a81bc516a314ca
jantapabh <s6035512034@phuket.psu.ac.th>
2020-06-01 16:12:10 +0700

    add description

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -21,7 +21,6 @@ export class HeroService {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
   
-
   //ทำการ inject message service เพื่อใช้งาน จะดึง add กับ clear มาเพื่อให้ทำงานได้
   constructor(private http: HttpClient, private messageService: MessageService) { }
 
